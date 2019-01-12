16e8941b8595a77f8a3045c8e52b2def9e6f0ca5
dnc77 <44986862+dnc77@users.noreply.github.com>
2019-01-12 21:56:02 +1100

    Add files via upload
    12 Jan 2019
    * Added main hideout files main.css and index.php.

---
+++ hideout/index.php
@@ -0,0 +1,17 @@
+<?
+   // Reset any previous sessions.
+   session_start();
+   session_unset();
+   session_destroy();
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+   <link rel="stylesheet" type="text/css" href="main.css"></link>
+</head>
+
+<body>
+<p><a href=omx/omx.php>omx</a></p>
+</body>
+</html>
